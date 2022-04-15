ff87fa54bd62ff64551a3f1e4dffccd8ba90cef1
bergstenarn <andersbergsten@yahoo.se>
2022-04-15 07:08:13 +0300

    160. Connecting with the Latest Version of Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -21,10 +21,7 @@ mongoose.connection.on("error", (err) => {
 });
 
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
