1c4a7375be9cd51285d07b3e283101d9453ba052
bergstenarn <andersbergsten@yahoo.se>
2022-04-15 07:40:14 +0300

    159. Connecting to MongoDB - Correction

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const http = require("http");
 const mongoose = require("mongoose");
 
@@ -7,8 +8,7 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:Q5sa452_@nasacluster.my9ey.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGO_PW}@nasacluster.my9ey.mongodb.net/nasa?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
