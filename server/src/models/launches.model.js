20452b9fb79fa2c4dcd582c83e6f9fbe55df10dc
bergstenarn <andersbergsten@yahoo.se>
2022-04-19 21:20:47 +0300

    175. Referential Integrity

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,6 +1,5 @@
 const launchesDatabase = require("./launches.mongo");
-
-// const launches = new Map();
+const planets = require("./planets.mongo");
 
 let latestFlightNumber = 100;
 
@@ -9,13 +8,11 @@ const launch = {
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  target: "Kepler 442 b",
+  target: "Kepler-442 b",
   customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
-
-// launches.set(launch.flightNumber, launch);
 
 saveLaunch(launch);
 
@@ -23,15 +20,19 @@ function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-// function getAllLaunches() {
-//   return Array.from(launches.values());
-// }
-
 async function getAllLaunches() {
   return await launchesDatabase.find({}, { _id: 0, __v: 0 });
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
   await launchesDatabase.updateOne(
     {
       flightNumber: launch.flightNumber,
