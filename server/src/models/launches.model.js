0021db807d25467684273dce14dbc362ad0603ae
bergstenarn <andersbergsten@yahoo.se>
2022-04-22 12:18:33 +0300

    195. Persisting SpaceX Launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -43,6 +43,11 @@ async function populateLaunches() {
     },
   });
 
+  if (response.status !== 200) {
+    console.log("Problem downloading launch data");
+    throw new Error("Launch data download failed");
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc.payloads;
@@ -57,8 +62,10 @@ async function populateLaunches() {
       success: launchDoc.success,
       customers,
     };
+
     console.log(`${launch.flightNumber} ${launch.mission}`);
-    // TODO: Populate launches collection ...
+
+    await saveLaunch(launch);
   }
 }
 
@@ -101,14 +108,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error("No matching planet found");
-  }
-
   await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
@@ -121,6 +120,14 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
   const newFlightNumber = (await getLatestFlightNumber()) + 1;
 
   const newLaunch = Object.assign(launch, {
