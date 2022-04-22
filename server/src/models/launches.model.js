423eccae554b934c8856069e387f6b1f33b6b500
bergstenarn <andersbergsten@yahoo.se>
2022-04-22 08:29:42 +0300

    192. Mapping SpaceX Data To Our Database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,7 +20,7 @@ const DEFAULT_FLIGHT_NUMBER = 100;
 
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
-async function loadLaunchesData() {
+async function loadLaunchData() {
   console.log("Downloading launch data ...");
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
@@ -41,6 +41,23 @@ async function loadLaunchesData() {
       ],
     },
   });
+
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc.payloads;
+    const customers = payloads.flatMap((payload) => payload.customers);
+
+    const launch = {
+      flightNumber: launchDoc.flight_number,
+      mission: launchDoc.name,
+      rocket: launchDoc.rocket.name,
+      launchDate: launchDoc.date_local,
+      upcoming: launchDoc.upcoming,
+      success: launchDoc.success,
+      customers,
+    };
+    console.log(launch);
+  }
 }
 
 async function existsLaunchWithId(launchId) {
@@ -111,7 +128,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
-  loadLaunchesData,
+  loadLaunchData,
   existsLaunchWithId,
   getAllLaunches,
   scheduleNewLaunch,
